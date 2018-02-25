28d1108df6d5f62fa73b0019756edd38dc386df3
asmw <asmw@asmw.org>
2018-02-25 15:20:33 +0100

    The args are owned by the CmdLn so they live long enough

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ mod error;
 
 pub struct CmdLn {
     raw: *mut bindings::cmd_ln_t,
+    c_args: Vec<CString>,
 }
 
 impl CmdLn {
@@ -35,7 +36,10 @@ impl CmdLn {
         if raw.is_null() {
             return Err(Error);
         }
-        Ok(CmdLn{raw: raw})
+        Ok(CmdLn{
+            raw: raw,
+            c_args: c_args,
+        })
     }
 
     pub unsafe fn get_str(&self, name: &str) -> &str {
